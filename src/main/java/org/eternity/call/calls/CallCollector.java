61dcbb9a1beba08f11da4533c70bf26c58938e53
조영호 <aeternum9342@gmail.com>
2025-05-08 05:25:56 +0900

    오브젝트 설계 원칙편 8-3 예제

---
+++ src/main/java/org/eternity/call/calls/CallCollector.java
@@ -0,0 +1,26 @@
+package org.eternity.call.calls;
+
+import java.util.List;
+
+public class CallCollector implements Collector {
+    private Reader reader;
+
+    public CallCollector(Reader reader) {
+        this.reader = reader;
+    }
+
+    @Override
+    public CallHistory collect(String phone) {
+        CallHistory history = new CallHistory(phone);
+
+        List<Call> calls = reader.read();
+
+        for(Call call : calls) {
+            if (call.from().equals(phone)) {
+                history.append(call);
+            }
+        }
+
+        return history;
+    }
+}
